da19c42118d105ce67fc5fbe790d9cef300f3837
andre <amcmorris@deckerip.com>
2016-04-12 09:00:04 -0400

    first commit

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,30 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    for(var i =0, len = arr.length; i< len;i++){
+      if(arr[i] == item){
+        return i;
+      }
+    }
+    return -1;
   },
 
   sum : function(arr) {
-
+    var sum = 0;
+    for(var i =0, len = arr.length; i< len;i++){
+      sum += arr.pop(i);
+    }
+    return sum;
   },
 
   remove : function(arr, item) {
-
+    for(var i = 0; i< arr.length;i++){
+      if(arr[i] == item){
+        console.log("true");
+        arr.splice(i,1);
+      }  
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
