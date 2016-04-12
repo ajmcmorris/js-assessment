de1de645a34c0ed20b218660c81fd3dfdafdcea3
andre <amcmorris@deckerip.com>
2016-04-12 10:42:52 -0400

    array test progress update

--- app/arrays.js
+++ app/arrays.js
@@ -22,7 +22,6 @@ exports.arraysAnswers = {
   remove : function(arr, item) {
     for(var i = 0; i< arr.length;i++){
       if(arr[i] == item){
-        console.log("true");
         arr.splice(i,1);
       }  
     }
@@ -30,27 +29,41 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy : function(arr, item) {
+    var arr_cp = [];
 
+    console.log(arr);
+    for(var i = 0,len = arr.length; i< len;i++){
+      if(arr[i] == item){
+        arr.splice(i,2);
+      }  
+    }
+    console.log(arr);
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    var new_arr = arr1.concat(arr2);
+    return new_arr;
   },
 
   insert : function(arr, item, index) {
